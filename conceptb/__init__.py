de8d37c06061becb00e23ebd45f76c848f320d5c
Rami El Mallah <elmallahrami@gmail.com>
2022-08-30 19:58:36 +0300

    mend

--- conceptb/__init__.py
+++ conceptb/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-# from conceptb.models import User, Order
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '7692j479263qj972z3694329ja9jzoqopiwealp82'
@@ -13,10 +12,12 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
-# if len(User.query.filter_by(is_admin=True).all()) == 0:
-#     admin = User(email='admin@admin.com', phone='03123456', location='ConceptB', password='', is_admin=True)
-#     order = Order(status='In progress', user=admin)
-#     db.session.add_all([admin, order])
-#     db.session.commit()
+from conceptb.models import User, Order
+
+if len(User.query.filter_by(is_admin=True).all()) == 0:
+    admin = User(email='admin@admin.com', phone='03123456', location='ConceptB', password='$2b$12$koQjI10KBhcXUhEND/Xoe.Jgr4G90IDCF17csMzAKo.4YShisRCSm', is_admin=True)
+    order = Order(status='In progress', user=admin)
+    db.session.add_all([admin, order])
+    db.session.commit()
 
 from conceptb import routes
