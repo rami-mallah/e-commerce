7208cbea8192984324fd22713420ce5704737c94
Rami El Mallah <elmallahrami@gmail.com>
2022-08-30 21:45:22 +0300

    Made changes for heroku

--- conceptb/__init__.py
+++ conceptb/__init__.py
@@ -5,7 +5,8 @@ from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = '7692j479263qj972z3694329ja9jzoqopiwealp82'
-app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+#app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///site.db'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'postgres://xkfsmnufmedxpz:20da52d446f0586861b77b33ad96555412aac11796550dab7a50d570c8378e72@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d1501ikq99489h'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
@@ -14,10 +15,10 @@ login_manager.login_message_category = 'info'
 
 from conceptb.models import User, Order
 
-if len(User.query.filter_by(is_admin=True).all()) == 0:
-    admin = User(email='admin@admin.com', phone='03123456', location='ConceptB', password='$2b$12$koQjI10KBhcXUhEND/Xoe.Jgr4G90IDCF17csMzAKo.4YShisRCSm', is_admin=True)
-    order = Order(status='In progress', user=admin)
-    db.session.add_all([admin, order])
-    db.session.commit()
+# if len(User.query.filter_by(is_admin=True).all()) == 0:
+#     admin = User(email='admin@admin.com', phone='03123456', location='ConceptB', password='$2b$12$koQjI10KBhcXUhEND/Xoe.Jgr4G90IDCF17csMzAKo.4YShisRCSm', is_admin=True)
+#     order = Order(status='In progress', user=admin)
+#     db.session.add_all([admin, order])
+#     db.session.commit()
 
 from conceptb import routes
